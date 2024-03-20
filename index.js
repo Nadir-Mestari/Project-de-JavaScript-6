c55a89c396d8572f017d9d0af3ba43bde5f8f87c
Nadir <contact@mestarico.com>
2024-03-20 08:22:53 +0100

    Finish

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
 
-// Ranger la sidebar si on click sur le contenu principal
+const sidebar = document.getElementById("side-bar")
+const content = document.querySelector(".content")
 
-// BONUS
-// Créer en css l'hamburger menu qui se transforme en croix
 
-// Aide
-// Priorités en CSS : id > classe > baliseHtml
+btn.addEventListener("click", () => {
+
+    sidebar.classList.toggle("btn-remove")
+
+
+
+})
+
+content.addEventListener("click", () => {
+    sidebar.classList.remove("btn-remove")
+})
